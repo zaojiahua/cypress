ed0931b0055d8203b41db7679cbba908cd9870a2
darr_en1 <darr_en1@126.com>
2019-05-20 20:01:41 +0800

    设置路径引入方式

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,16 @@
+const BASE_URL = process.env.NODE_ENV === 'production' ? '/production/' : '/'
+
+const path = require('path')
+
+const resolve = dir => path.join(__dirname, dir)
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  baseUrl: BASE_URL,
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('_c', resolve('src/components'))
+  },
+  productionSourceMap: false
 }
