52a2dfe73887ec3c8a665f6fb6d26e9a7ea9c8b4
darr_en1 <darr_en1@126.com>
2019-05-26 18:03:53 +0800

    add 404 page

---
+++ src/components/notFoundComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <img src="../assets/image/404.jpg" class="img-responsive">
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+  .img-responsive {
+    display: inline-block;
+    height: auto;
+    max-width: 100%;
+  }
+</style>
