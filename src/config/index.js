4eac96d755723dccc998b9269c96689c233bf3fa
darr_en1 <darr_en1@126.com>
2019-06-10 16:08:15 +0800

    添加.env

--- src/config/index.js
+++ src/config/index.js
@@ -1,14 +1,2 @@
-import ip from './ip'
-export default {
-  'REEF_HOST': ip.REEF_HOST,
-  'REEF_PORT': '8000',
-  'CORAL_HOST': ip.CORAL_HOST,
-  'ADMIN_PORT': '10803',
-  'CONFIG_PORT': '10808',
-  'DEVINDOOR_PORT': '10802',
-  'CREATETBOARD_PORT': '10811',
-  'JOBSVC_PORT': '10801'
-}
-
-export const baseURL = process.env.NODE_ENV === 'production' ? '/production/' : `http://${ip.REEF_HOST}:${ip.REEF_PORT}`
-export const jobLibSvcURL = `http://${ip.CORAL_HOST}:${ip.JOBSVC_PORT}`
+export const baseURL = process.env.NODE_ENV === 'production' ? '/production/' : `http://${process.env.VUE_APP_REEF_HOST}:${process.env.VUE_APP_REEF_PORT}`
+export const jobLibSvcURL = `http://${process.env.VUE_APP_CORAL_HOST}:${process.env.VUE_APP_JOBSVC_PORT}`
