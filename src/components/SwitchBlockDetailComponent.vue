1d9aecd9af63900f330695e2b9818e9bcb256884
darr_en1 <darr_en1@126.com>
2019-06-17 11:31:59 +0800

    switchBlock msg edit validate finish

--- src/components/SwitchBlockDetailComponent.vue
+++ src/components/SwitchBlockDetailComponent.vue
@@ -4,14 +4,14 @@
     :value="switchBlockModalShow"
     :mask-closable="false"
     :closable="false">
-    <Form ref="switchBlockForm" :label-width="80" :model="switchBlockInfo">
-      <FormItem label="name">
+    <Form ref="switchBlockForm" :label-width="80" :model="switchBlockInfo" :rules="switchBlocRules">
+      <FormItem label="name" prop="switchBlockName">
         <Input v-model="switchBlockInfo.switchBlockName" placeholder="Enter something..."/>
       </FormItem>
-      <FormItem label="fileName">
+      <FormItem label="fileName" prop="fileName">
         <Input v-model="switchBlockInfo.fileName" placeholder="Enter something..."/>
       </FormItem>
-      <FormItem label="explain">
+      <FormItem label="explain" prop="explain">
         <Input v-model="switchBlockInfo.explain" type="textarea" :autosize="{minRows: 2,maxRows: 5}" placeholder="Enter something..." />
       </FormItem>
     </Form>
@@ -37,8 +37,28 @@ export default {
     }
   },
   data () {
+    const validatePassCheck = (rule, value, callback) => {
+      if (!value) {
+        callback(new Error('文件名不能为空'))
+      } else if (!new RegExp('(.py|.txt)$').test(value)) {
+        callback(new Error('文件名后缀必须为 .py|.txt'))
+      } else {
+        callback()
+      }
+    }
     return {
-      switchBlockRules: ''
+      switchBlockRules: '',
+      switchBlocRules: {
+        switchBlockName: [
+          { required: true, message: 'switchBlock名称不能为空', trigger: 'blur' }
+        ],
+        fileName: [
+          { validator: validatePassCheck, trigger: 'blur' }
+        ],
+        explain: [
+          { required: true, message: '描述不能为空', trigger: 'blur' }
+        ]
+      }
     }
   },
   model: {
@@ -47,8 +67,14 @@ export default {
   },
   methods: {
     submit () {
-      this.$emit('close', false)
-      this.$emit('save', this.switchBlockInfo)
+      this.$refs.switchBlockForm.validate((valid) => {
+        if (valid) {
+          this.$emit('close', false)
+          this.$emit('save', this.switchBlockInfo)
+        } else {
+          this.$Message.error('Fail!')
+        }
+      })
     },
     cancel () {
       this.$emit('close', false)
