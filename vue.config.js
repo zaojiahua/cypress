6979e7a94df64106d86227a322205494e3ad0eb8
darr_en1 <darr_en1@126.com>
2019-06-18 10:29:16 +0800

    Change the filename generated by the packaging to cypress_vue

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,7 @@ const resolve = dir => path.join(__dirname, dir)
 module.exports = {
   lintOnSave: false,
   publicPath: BASE_URL,
+  outputDir: 'cypress_vue',
   chainWebpack: config => {
     config.resolve.alias
       .set('@', resolve('src'))
