31388c7f209e0b69a48fe6412b3b65bd6eb4aebf
darr_en1 <darr_en1@126.com>
2019-08-06 14:57:09 +0800

    add depend lodbash

--- src/config/dev.env.js
+++ src/config/dev.env.js
@@ -1,4 +1,4 @@
 export default {
-  'REEF_HOST': '10.80.3.138',
-  'CORAL_HOST': '10.80.3.100'
+  'REEF_HOST': '10.80.6.138',
+  'CORAL_HOST': '10.80.6.100'
 }
--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import router from './router'
 import store from './store'
 import './plugins/iview.js'
 import { MOCK } from './config'
-if (process.env.NODE_ENV !== 'production' & MOCK) require('./mock')
+import lodash from 'lodash'
+
+if (process.env.NODE_ENV !== 'production' && MOCK) require('./mock')
 Vue.config.productionTip = false
+
+Vue.prototype._ = lodash
 
 new Vue({
   router,
