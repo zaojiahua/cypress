3d195b92f23677652b41e932129b6ec9440586ee
lc <lc@anhereef.com>
2020-09-07 15:22:05 +0800

    添加了断网/联网提醒

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,6 +12,22 @@ export default {
   name: 'home',
   components: {
     layout
+  },
+  mounted () {
+    window.addEventListener('online', () => {
+      this.$Notice.destroy()
+      this.$Message.success({
+        background: true,
+        content: '网络连接已恢复'
+      })
+    })
+    window.addEventListener('offline', () => {
+      this.$Notice.error({
+        title: '警告',
+        desc: '网络连接已断开，请检查您的网络',
+        duration: 0
+      })
+    })
   }
 }
 </script>
