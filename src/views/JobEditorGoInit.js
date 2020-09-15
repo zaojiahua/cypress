07e4427671d7d0437fe66e826ad5a891a1e6cacd
lc <lc@anhereef.com>
2020-09-15 10:48:17 +0800

    添加了编辑Unit模板时的权限提醒功能

--- src/views/JobEditorGoInit.js
+++ src/views/JobEditorGoInit.js
@@ -255,7 +255,14 @@ export function innerDiagramInit (context) {
   }
 
   unitTemplate.contextClick = function (e, node) {
-    if (e.diagram instanceof go.Palette && sessionStorage.identity.includes('Admin')) {
+    if (e.diagram instanceof go.Palette) {
+      if (!sessionStorage.identity.includes('Admin')) {
+        context.$Notice.warning({
+          title: '温馨提醒',
+          desc: '该功能仅限管理员使用，请切换您的账号或重新登录。'
+        })
+        return
+      }
       context.isDiagram = false
       context.unitTemplateName = node.data.text
       context.unitTemplateId = node.data.unit_id
