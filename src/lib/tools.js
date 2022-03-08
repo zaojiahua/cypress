8b6dd7287ea531f005df82d469a3fde211c6a341
ltt <ltt@wtovc.com>
2022-03-08 14:55:42 +0800

    保存图片到本地( 全部保存为png格式 )

--- src/lib/tools.js
+++ src/lib/tools.js
@@ -104,14 +104,14 @@ function blobToDataURL (blob) {
 /// DataUrl 转 Blob
 function dataURLtoBlob (dataUrl){
   let arr = dataUrl.split(',')
-  let mime = arr[0].match(/:(.*?);/)[1]
+  // let mime = arr[0].match(/:(.*?);/)[1]
   let bstr = atob(arr[1])
   let n = bstr.length
   let u8arr = new Uint8Array(n)
   while(n--){
     u8arr[n] = bstr.charCodeAt(n)
   }
-  return new Blob([u8arr],{type:mime})
+  return new Blob([u8arr],{type:'image/png'})
 }
 
 function prefixRemove (prefix, child, data) {
